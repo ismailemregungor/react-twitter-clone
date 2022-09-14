3ec0b16c22a8743a33afe71e0c983a8373a137eb
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-14 21:16:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import Container from './layout/Container';
+import Sidebar from './layout/Sidebar';
+import Timeline from './layout/Timeline';
+import Widgets from './layout/Widgets';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container>
+        <Sidebar />
+        <Widgets />
+        <Timeline />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
