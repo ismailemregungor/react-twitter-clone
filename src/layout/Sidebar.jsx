b5049fd4d2a2d26b1e6e668798bc5706d7c44513
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-15 17:49:44 +0300

    Update Sidebar.jsx

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import {
+  BookmarksIcon,
+  CommunitiesIcon,
+  ExploreIcon,
+  HomeIcon,
+  ListsIcon,
+  Logo,
+  MessagesIcon,
+  MoreIcon,
+  NotificationsIcon,
+  ProfileIcon,
+} from '../assets/icons/icon';
 
 const Sidebar = () => {
-  return <div className="w-72 min-h-screen border">Sidebar</div>;
+  return (
+    <div className="w-72 min-h-screen border">
+      <h1>Sidebar</h1>
+      <Logo />
+      <HomeIcon />
+      <ExploreIcon />
+      <CommunitiesIcon />
+      <NotificationsIcon />
+      <MessagesIcon />
+      <BookmarksIcon />
+      <ProfileIcon />
+      <MoreIcon />
+    </div>
+  );
 };
 
 export default Sidebar;
