604b2f0194f3b51e34f9be1ed437b31504e21e34
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-17 17:50:02 +0300

    Update Widgets.jsx

--- src/layout/Widgets.jsx
+++ src/layout/Widgets.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import {
+  EmojiIcon,
+  GIFIcon,
+  ImageIcon,
+  LikeIcon,
+  LocationIcon,
+  PollIcon,
+  PopulerIcon,
+  ReplyIcon,
+  RetweetIcon,
+  ScheduleIcon,
+  ShareIcon,
+} from '../assets/icons/Icon';
 
 const Widgets = () => {
-  return <div className="flex-1 min-h-screen border">Main</div>;
+  return (
+    <div className="flex-1 min-h-screen border">
+      <h1>Main</h1>
+      <PopulerIcon />
+      <ImageIcon />
+      <GIFIcon />
+      <PollIcon />
+      <EmojiIcon />
+      <ScheduleIcon />
+      <LocationIcon />
+      <ReplyIcon />
+      <RetweetIcon />
+      <LikeIcon />
+      <ShareIcon />
+    </div>
+  );
 };
 
 export default Widgets;
