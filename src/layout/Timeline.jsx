7759e05207aa46e0c843a006511aa90411870c2a
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-17 17:51:15 +0300

    Update Timeline.jsx

--- src/layout/Timeline.jsx
+++ src/layout/Timeline.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Timeline = () => {
-  return <div className="w-80 min-h-screen border">Timeline</div>;
+  return (
+    <div className="w-80 min-h-screen border">
+      <h1>Timeline</h1>
+    </div>
+  );
 };
 
 export default Timeline;
