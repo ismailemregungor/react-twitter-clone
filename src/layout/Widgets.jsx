bf593cafcf0f400f6fb7e65fda5cfa199b3f27ee
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2022-09-21 00:06:01 +0300

    Update Widgets.jsx

--- src/layout/Widgets.jsx
+++ src/layout/Widgets.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SearchIcon } from '../assets/icons/Icon';
+import { Timeline } from 'react-twitter-widgets';
 
-const Timeline = () => {
+const Widgets = () => {
   return (
-    <div className="w-80 min-h-screen border">
+    <div className="w-80 min-h-screen">
       <div className="flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
         <SearchIcon className="" />
         <input
@@ -11,9 +12,19 @@ const Timeline = () => {
           className="placeholder-gray-dark bg-transparent outline-none w-full text-sm"
         />
       </div>
-      <div>Timeline</div>
+      <div className="mt-5">
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'reactjs',
+          }}
+          options={{
+            height: '600',
+          }}
+        />
+      </div>
     </div>
   );
 };
 
-export default Timeline;
+export default Widgets;
